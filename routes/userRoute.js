3e58c6d10d591ec336238bfde2e3cf06ab8aca8f
unknown <isratjahan.emu1998@gmail.com>
2023-02-06 00:15:20 +0600

    get user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {getAllUser} = require("../controllers/user/getAlluser");
 const {updateUser} = require("../controllers/user/updateUser");
 const router = express.Router();
 const {userLogin} = require("../controllers/user/userLogin");
@@ -8,7 +9,7 @@ router.post("/addUser", userSignUp);
 router.post("/authenticateUser", userLogin);
 // router.delete("/deleteUser/:email", deleteUser);
 // router.get("/findUser/:email", findUserByEmail);
-// router.get("/allusers", getAllUser);
+router.get("/allUsers", getAllUser);
 router.post("/updateUser/:email", updateUser);
 
 module.exports = router;
