4dc8682bad25c1562a8989f6079eaad54fcbff2d
Tushar@8640 <imtantushar44@gmail.com>
2023-02-13 22:35:08 +0600

    update profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,23 @@
 const express = require("express");
-const {createOrRemoveAdmin} = require("../controllers/user/createOrRemoveAdmin");
-const {deleteUser} = require("../controllers/user/deleteUser");
-const {getAllUser} = require("../controllers/user/getAlluser");
-const {getFilteredUser} = require("../controllers/user/getFilteredUser");
-const {statusUpdate} = require("../controllers/user/statusUpdate");
-const {updateUser} = require("../controllers/user/updateUser");
+const {
+  createOrRemoveAdmin,
+} = require("../controllers/user/createOrRemoveAdmin");
+const { deleteUser } = require("../controllers/user/deleteUser");
+const { getAllUser } = require("../controllers/user/getAlluser");
+const { getFilteredUser } = require("../controllers/user/getFilteredUser");
+const { getSingleUser } = require("../controllers/user/getSingleUser");
+const { statusUpdate } = require("../controllers/user/statusUpdate");
+const { updateUser } = require("../controllers/user/updateUser");
 const router = express.Router();
-const {userLogin} = require("../controllers/user/userLogin");
-const {userSignUp} = require("../controllers/user/userSignUp");
+const { userLogin } = require("../controllers/user/userLogin");
+const { userSignUp } = require("../controllers/user/userSignUp");
 
 router.post("/addUser", userSignUp);
 router.post("/authenticateUser", userLogin);
 router.post("/changeAdmin", createOrRemoveAdmin);
 router.post("/statusUpdate", statusUpdate);
 router.delete("/deleteUser/:email", deleteUser);
-// router.get("/findUser/:email", findUserByEmail);
+router.get("/getSingleUser/:email", getSingleUser);
 router.get("/allUsers", getAllUser);
 router.post("/filteredUsers", getFilteredUser);
 router.post("/updateUser/:email", updateUser);
