1169ba97d685f94ffc93f43f64aeff1ae24fcfab
Tushar@8640 <imtantushar44@gmail.com>
2023-03-11 11:43:37 +0600

    socket is connected

--- index.js
+++ index.js
@@ -8,6 +8,49 @@ db()
 
 
 // create server 
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`---Server is Listening On PORT ::: ${PORT}`);
 })
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Sockets are in");
+    socket.on("setup", (userData) => {
+      // console.log(userData)
+      socket.emit("connected",userData);
+    });
+
+    socket.on("newNotification", (data) => {
+      console.log("from socket",data)
+      io.emit("getNotification",data)
+    });
+    // socket.on("join chat", (room) => {
+    //   socket.join(room);
+    //   console.log("User joined room: " + room);
+    // });
+    // socket.on("new message", (newMessage) => {
+    //   var chat = newMessage.chatId;
+    //   if (!chat.users) return console.log("chat.users not defined");
+  
+    //   chat.users.forEach((user) => {
+    //     if (user._id === newMessage.sender._id) return;
+    //     socket.in(user._id).emit("message received", newMessage);
+    //   });
+    //   socket.on("typing", (room) => {
+    //     socket.in(room).emit("typing");
+    //   });
+    //   socket.on("stop typing", (room) => {
+    //     socket.in(room).emit("stop typing");
+    //   });
+    // });
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+    });
+  });
